56c064a199e33f5093ce7b10a103d040c9adb816
Kevin Martin <dev.kmartin@gmail.com>
2025-02-27 11:17:38 +0100

    Initial commit

---
+++ src/test/java/nl/kmartin/dartsmatcherapiv2/DartsMatcherApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.kmartin.dartsmatcherapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartsMatcherApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
