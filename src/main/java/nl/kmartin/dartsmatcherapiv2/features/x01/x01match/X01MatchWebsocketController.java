4e21d79f740ac6718ef0811f97fc0f7f63166a3e
Kevin Martin <dev.kmartin@gmail.com>
2025-03-21 15:28:16 +0100

    feat: add WebSocket destinations for add turn, edit turn, delete turn, delete leg, delete set, and match subscription

---
+++ src/main/java/nl/kmartin/dartsmatcherapiv2/features/x01/x01match/X01MatchWebsocketController.java
@@ -0,0 +1,66 @@
+package nl.kmartin.dartsmatcherapiv2.features.x01.x01match;
+
+import jakarta.validation.Valid;
+import nl.kmartin.dartsmatcherapiv2.features.x01.model.*;
+import nl.kmartin.dartsmatcherapiv2.utils.WebsocketDestinations;
+import org.bson.types.ObjectId;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+
+@Controller
+public class X01MatchWebsocketController {
+
+    private final IX01MatchService x01MatchService;
+    private final IX01MatchWebsocketService x01MatchWebsocketService;
+
+    public X01MatchWebsocketController(IX01MatchService x01MatchService, IX01MatchWebsocketService x01MatchWebsocketService) {
+        this.x01MatchService = x01MatchService;
+        this.x01MatchWebsocketService = x01MatchWebsocketService;
+    }
+
+    @SubscribeMapping(WebsocketDestinations.X01_GET_MATCH)
+    public X01Match subscribeX01Match(@DestinationVariable ObjectId matchId) {
+
+        return x01MatchService.getMatch(matchId);
+    }
+
+    @MessageMapping(WebsocketDestinations.X01_ADD_TURN)
+    public X01Match addTurn(@Valid @Payload X01Turn x01Turn) throws IOException {
+        X01Match updatedMatch = x01MatchService.addTurn(x01Turn);
+        x01MatchWebsocketService.sendX01MatchUpdate(updatedMatch);
+        return updatedMatch;
+    }
+
+    @MessageMapping(WebsocketDestinations.X01_EDIT_TURN)
+    public X01Match editTurn(@Valid @Payload X01EditTurn x01EditTurn) throws IOException {
+        X01Match updatedMatch = x01MatchService.editTurn(x01EditTurn);
+        x01MatchWebsocketService.sendX01MatchUpdate(updatedMatch);
+        return updatedMatch;
+    }
+
+    @MessageMapping(WebsocketDestinations.X01_DELETE_TURN)
+    public X01Match deleteTurn(@Valid @Payload X01DeleteTurn x01DeleteTurn) {
+        X01Match updatedMatch = x01MatchService.deleteTurn(x01DeleteTurn);
+        x01MatchWebsocketService.sendX01MatchUpdate(updatedMatch);
+        return updatedMatch;
+    }
+
+    @MessageMapping(WebsocketDestinations.X01_DELETE_LEG)
+    public X01Match deleteLeg(@Valid @Payload X01DeleteLeg x01DeleteLeg) {
+        X01Match updatedMatch = x01MatchService.deleteLeg(x01DeleteLeg);
+        x01MatchWebsocketService.sendX01MatchUpdate(updatedMatch);
+        return updatedMatch;
+    }
+
+    @MessageMapping(WebsocketDestinations.X01_DELETE_SET)
+    public X01Match deleteSet(@Valid @Payload X01DeleteSet x01DeleteSet) {
+        X01Match updatedMatch = x01MatchService.deleteSet(x01DeleteSet);
+        x01MatchWebsocketService.sendX01MatchUpdate(updatedMatch);
+        return updatedMatch;
+    }
+}
