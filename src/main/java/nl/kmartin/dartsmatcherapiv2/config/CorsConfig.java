dbc421ba2923e35781ecbdacf2085216f2a0fd8c
Kevin Martin <dev.kmartin@gmail.com>
2025-07-17 12:52:20 +0200

    chore: add web app domain to cors config

--- src/main/java/nl/kmartin/dartsmatcherapiv2/config/CorsConfig.java
+++ src/main/java/nl/kmartin/dartsmatcherapiv2/config/CorsConfig.java
@@ -1,30 +1,41 @@
 package nl.kmartin.dartsmatcherapiv2.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:4200",
+                "https://jiangxy.github.io/websocket-debug-tool/",
+                "http://192.168.1.*:4200",
+                "https://dartsmatcherv2.kmartin.nl"
+        ));
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns(
-                                "http://localhost:4200",
-                                "https://jiangxy.github.io/websocket-debug-tool/",
-                                "http://192.168.1.*:4200"
-                        )
-                        .allowedOriginPatterns("http://192.168.1.*:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean(UrlBasedCorsConfigurationSource source) {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
